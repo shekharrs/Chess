f8fe2a9bd4f9ab3b3b20a211bd57e52077b28ba5
shekhar <shekhart442@gmail.com>
2024-12-07 21:09:54 +0530

    first commit

---
+++ public/javascripts/chessgame.js
@@ -0,0 +1,14 @@
+const socket = io();
+const chess = new Chess();
+
+const boardElement = document.querySelector(".chessboard")
+
+let draggedPiece = null;
+let sourceSquare = null;
+let playerRole = null;
+
+const renderBoard = () => {};
+
+const handleMove = () => {};
+
+const getPieceUnicode = () => {}
